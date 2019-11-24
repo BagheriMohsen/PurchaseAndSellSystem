f409f1c93b8ed73a1646ddc60863e35c7fbb2a33
mortezaMohiuodin <mortezamohiuodin@gmail.com>
2019-11-24 17:28:42 +0330

    Adding functions to custom.js for date converting to jalali and adding .timestamp class to date cells in tables

--- public/AdminPanel/dist/js/custom.js
+++ public/AdminPanel/dist/js/custom.js
@@ -649,5 +649,51 @@ $(document).ready(function(){
     });
     // Setup persian datepicker for date inputs
     $(".persianDatePicker").pDatepicker();
+
+    //Add zero to hours,minutes and seconds for clock
+    function addZero(i) {
+        if (i < 10) {
+          i = "0" + i;
+        }
+        return i;
+    }
+    //Turn timestamp js date object
+    function convertTimeStampToJalali(timestamp){
+        var date = new Date(timestamp);
+        if(!date)
+            return false;
+            return ( gregorian_to_jalali(date.getFullYear(),(date.getMonth()+1),date.getDate(),date.getHours(),date.getMinutes(),date.getSeconds()) );
+    }
+    // Turn js date object to jalali date
+    function gregorian_to_jalali(gy,gm,gd,h,m,s){
+        g_d_m=[0,31,59,90,120,151,181,212,243,273,304,334];
+        if(gy > 1600){
+            jy=979;
+            gy-=1600;
+        }else{
+            jy=0;
+            gy-=621;
+        }
+        gy2=(gm > 2)?(gy+1):gy;
+        days=(365*gy) +(parseInt((gy2+3)/4)) -(parseInt((gy2+99)/100)) +(parseInt((gy2+399)/400)) -80 +gd +g_d_m[gm-1];
+        jy+=33*(parseInt(days/12053)); 
+        days%=12053;
+        jy+=4*(parseInt(days/1461));
+        days%=1461;
+        if(days > 365){
+            jy+=parseInt((days-1)/365);
+            days=(days-1)%365;
+        }
+        jm=(days < 186)?1+parseInt(days/31):7+parseInt((days-186)/30);
+        jd=1+((days < 186)?(days%31):((days-186)%30));
+        var hours = addZero(h);
+        var minutes = addZero(m);
+        var seconds = addZero(s);
+        return jy +'/' + jm + '/' + jd + ' ' + hours + ":" + minutes + ":" + seconds;
+    }
+    $('.timestamp').each(function(index,value){
+        var jalaliTime = convertTimeStampToJalali(value.innerHTML)
+        value.innerHTML = (jalaliTime);
+    });
 });
 
