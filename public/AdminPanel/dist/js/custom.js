30648d7b4b434bc218e8c167e76bdeae3df3ebdc
mortezaMohiuodin <mortezamohiuodin@gmail.com>
2019-11-30 03:13:16 +0330

    Adding comma seprator for numbers every page and form

--- public/AdminPanel/dist/js/custom.js
+++ public/AdminPanel/dist/js/custom.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+    //Remove comma from number inputs for product Create Form
     $('#productCreateForm').submit(function(event){
+        event.preventDefault();
+        $('input.comma').each(function(index,item){
+            item.value = parseInt(item.value.replace(/\,/g,'',10));
+        });
+        $(this)[0].submit();
+    });
+    //Remove comma from number inputs for product Edit Form
+    $('#productEditForm').submit(function(event){
         event.preventDefault();
         $('input.comma').each(function(index,item){
             item.value = parseInt(item.value.replace(/\,/g,'',10));
@@ -314,7 +323,7 @@ $(document).ready(function(){
         <tr class="text-center">
             
         <td>${product_name}</td>
-        <th>${newSpecial.price}</th>
+        <th>${numberWithCommas(newSpecial.price)}</th>
         <th>${newSpecial.place}</th>
         <td>
           <form  action="http://localhost:8000/special-tariffs/${newSpecial.id}" method="post" >
@@ -366,6 +375,9 @@ $(document).ready(function(){
     $('.storeSTariffForm').submit(function(event){
         event.preventDefault();
         var self = $(this);
+        $('input.comma').each(function(index,item){
+            item.value = parseInt(item.value.replace(/\,/g,'',10));
+        });
         $(this).find('button[type="submit"]').html('<i class="fas fa-spinner"></i>');
         var actionUrl = $(this).attr('action');
         var CSRF_TOKEN = $(this).find('input[name="_token"]').val();
@@ -533,7 +545,7 @@ $(document).ready(function(){
                     });
                 }
             });
-            productPriceInput.value = productPrice;
+            productPriceInput.value = numberWithCommas(productPrice);
 
         }else if(event.target.classList.contains('fa-trash-alt')){
             // event.target.parentElement.parentElement.parentElement.parentElement.remove();
@@ -579,9 +591,9 @@ $(document).ready(function(){
         var HBD_Date = form.find('input[name="HBD_Date"]').val();
         var address = form.find('textarea[name="address"]').val();
         var paymentMethod = form.find('input[name="paymentMethod"]').val();
-        var shippingCost = form.find('input[name="shippingCost"]').val();
-        var cashPrice = form.find('input[name="cashPrice"]').val();
-        var prePayment = form.find('input[name="prePayment"]').val();
+        var shippingCost = form.find('input[name="shippingCost"]').val().replace(/\,/g,'',10);
+        var cashPrice = form.find('input[name="cashPrice"]').val().replace(/\,/g,'',10);
+        var prePayment = form.find('input[name="prePayment"]').val().replace(/\,/g,'',10);
         var chequePrice = form.find('input[name="chequePrice"]').val();
         var instant = form.find('input[name="instant"]').val();
         var sellerDescription = form.find('textarea[name="sellerDescription"]').val();
@@ -643,13 +655,13 @@ $(document).ready(function(){
             var rowPrice = null;
             var count = value.querySelector('input[name="count"]').value;
             var off = value.querySelector('input[name="off"]').value;
-            var price = value.querySelector('input[name="price"]').value;
+            var price = value.querySelector('input[name="price"]').value.replace(/\,/g,'',10);
             rowPrice = (count*price) - off;
             overallPrice += rowPrice;
         });
-        $('#overallPrice').html(overallPrice);
+        $('#overallPrice').html(numberWithCommas(overallPrice));
         // Cash price field updated after overallprice update
-        $('#orderForm input[name="cashPrice"]').val(overallPrice);
+        $('#orderForm input[name="cashPrice"]').val(numberWithCommas(overallPrice));
     });
     //Checking if city has agent exist when seller gives order
     $('#orderForm #city').on('click',function(){
@@ -708,6 +720,7 @@ $(document).ready(function(){
             .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         });
     });
+   
     var isoDate;
     // Setup persian datepicker for date inputs
     $(".persianDatePicker").pDatepicker({
@@ -832,7 +845,7 @@ $(document).ready(function(){
                 type:'GET',
                 success:function(response){
                     var price = parseInt(getProductPrice(response,product_id));
-                    var number = parseInt(self.find('input[name="number"]').val());
+                    var number = parseInt(self.find('input[name="number"]').val().replace(/\,/g,'',10));
                     number = number || 0;
                     self.find('.cargoValue').html(numberWithCommas(price*number));
                 }
@@ -1077,6 +1090,7 @@ $(document).ready(function(){
             });
         }
         
-    })
+    });
+  
 });
 
